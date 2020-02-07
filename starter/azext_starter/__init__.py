2dd383cf97f1a1bb360a473e517121729e438027
Eric Chen <erichen@microsoft.com>
2020-02-07 16:19:22 +0800

    add azure cli extension

---
+++ starter/azext_starter/__init__.py
@@ -0,0 +1,38 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core import AzCommandsLoader
+from knack.help_files import helps
+
+
+helps['starter'] = """
+    type: command
+    short-summary: Quick start on Azure services.
+    examples:
+        - name: Provision and deploy demo code for Azure services.
+          text: az starter 'webapp signalr' --resource-group rg
+"""
+
+
+class StarterCommandsLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+        custom_type = CliCommandType(operations_tmpl='azext_starter.custom#{}')
+        super(StarterCommandsLoader, self).__init__(
+            cli_ctx=cli_ctx, custom_command_type=custom_type)
+
+    def load_command_table(self, _):
+        with self.command_group('') as g:
+            g.custom_command('starter', 'start')
+        return self.command_table
+
+    def load_arguments(self, _):
+        with self.argument_context('starter') as c:
+            c.positional('service', help='Service you want to start, use space as delimiter for multiple services.')
+            c.argument('resource_group', options_list=['--resource-group', '-g'], help='Resouce group to provision services.')
+
+
+COMMAND_LOADER_CLS = StarterCommandsLoader
---
+++ starter/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
+]
+
+DEPENDENCIES = []
+
+setup(
+    name='starter',
+    version=VERSION,
+    description='Azure starter extension',
+    long_description='Help developer quick start on Azure services.',
+    license='MIT',
+    author='Eric Chen',
+    author_email='erichen@microsoft.com',
+    url='https://github.com/Azure/azure-cli-extensions/tree/master/src/starter',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(),
+    package_data={'azext_starter': ['azext_metadata.json']},
+    install_requires=DEPENDENCIES
+)
